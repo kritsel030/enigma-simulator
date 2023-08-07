c95e935a308a6cf5d1f7f31a31b9f7d6fca7665f
Kristel Nieuwenhuys <kristel@kramphub.com>
2023-08-07 22:03:33 +0200

    minor documentation

--- src/main/kotlin/bombe/sensingcircuit/DifferentialRelay.kt
+++ src/main/kotlin/bombe/sensingcircuit/DifferentialRelay.kt
@@ -4,14 +4,22 @@ import bombe.MainCircuit
 
 /*
 * Sensing relays in the Turing-Welchman bombe are so-called 'differential relays'.
-* Turing mentions the term 'differential relay' on 'Turing's Treatise on the Enigma', Chapter 6, page 106.
-* https://www.ugr.es/~aquiran/cripto/museo/turing/turchap6.pdf
 *
+* Turing mentions the term 'differential relay' when explaining the Bombe's
+* "mechanism for "distinguishes correct from incorrect positions".
+* Turing's Treatise on the Enigma, Chapter 6, page 110.
+* https://www.ugr.es/~aquiran/cripto/museo/turing/turchap6.pdf
+* "Normally current will flow through all the differential relays and they will not move.
+* When one reaches a position that might be correct the current fails to reach one of those relays,
+* and the current permanently flowing in the other coil of the relay causes it to close,
+* and bring the stopping mechanism into play. "
+*
+* Gordon Welchan doesn't use the term 'differential relay', but he does describe the workings of a 'sensing relay'
+* which fits the general description of a differential relay
 * Spider Number 3 - Goldon Welchman - Autumn 1940
 * https://www.joelgreenberg.co.uk/_files/ugd/bbe412_691c3b0212d64e4dbcc1ebf180bc50bd.pdf
 * from section "THE SENSING RELAYS":
-*
-* "Each sensing relay has two windings [a.k.a. 'coils], in opposite directions, and the relay is “up”, i.e.
+* "Each sensing relay has two windings [a.k.a. 'coils'], in opposite directions, and the relay is “up”, i.e.
 * the relay switches operate, when current passes through one coil but not through the other.
 * When current passes through both coils or through neither coil the switches do not operate,
 * and the relay is in its normal or “down” position."
@@ -20,12 +28,12 @@ open class DifferentialRelay protected constructor(val primaryCoil: Coil, val se
 {
     constructor(mainCircuit: MainCircuit) : this(Coil(mainCircuit), Coil(mainCircuit))
 
-    // true = current passes through one coil but not through the other
-    //          relay will pass current
-    //          relay operates | relay is "up" | relay moves
-    // false   = current passes through both coils or through neither
-    //          relay will not pass the current
-    //          relay does not operate | relay is "down" | relay does not move |
+    // true  = current passes through one coil but not through the other
+    //         relay will pass current
+    //         relay "operates" | relay is "up" | relay "moves"
+    // false = current passes through both coils or through neither
+    //         relay will not pass the current
+    //         relay does not operate | relay is "down" | relay does not move | relay is in its "normal" position
     fun operates() : Boolean {
         return primaryCoil.isEnergized().xor(secondaryCoil.isEnergized())    }
 }
